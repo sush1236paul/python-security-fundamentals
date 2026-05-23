8340344111ec9bfc1e05c03e20917b7ed3e953c7
sush1236paul <paulsushant003@gmail.com>
2026-05-23 22:58:12 +0530

    Add name verification feature in name_verification.py

---
+++ chapter-2/conditionals/name_verification.py
@@ -0,0 +1,9 @@
+name = input("Input your name for verification: ")
+
+print("Let me check your name in the database...")
+
+if name == "sushant":
+    print("You are present in the database")
+
+else:
+    print("Update the database")
