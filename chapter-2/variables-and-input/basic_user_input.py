eca47de004fac68f56b8fa49d4381b9568c87367
sush1236paul <paulsushant003@gmail.com>
2026-05-23 22:57:05 +0530

    Create basic_user_input.py

---
+++ chapter-2/variables-and-input/basic_user_input.py
@@ -0,0 +1,10 @@
+name = input("What's your name? ")
+print("Hello", name)
+
+age = input("What is your age? ")
+print("So your age is", age, "years old")
+
+operating_system = input("What is your favourite operating system? ")
+print("So it is", operating_system, "nice!!")
+
+print(f"Hello, {name}")
