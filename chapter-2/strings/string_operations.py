f9b3b08ea99845b6f0a486b93f4706403d65d70c
sush1236paul <paulsushant003@gmail.com>
2026-05-23 23:08:21 +0530

    Create string_operations.py

---
+++ chapter-2/strings/string_operations.py
@@ -0,0 +1,15 @@
+name = "Sushant Paul"
+
+print("Original string:", name)
+
+print("Uppercase:", name.upper())
+
+print("Lowercase:", name.lower())
+
+print("Length of string:", len(name))
+
+print("First character:", name[0])
+
+print("Replace operation:", name.replace("Paul", "Developer"))
+
+print("Split operation:", name.split())
