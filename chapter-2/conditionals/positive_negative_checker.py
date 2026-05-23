14aa001c81bc4e5afdd1fff562ef6ca5fcf800b9
sush1236paul <paulsushant003@gmail.com>
2026-05-23 23:00:06 +0530

    Create positive_negative_checker.py

---
+++ chapter-2/conditionals/positive_negative_checker.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number for positive or negative verification: "))
+
+if number > 0:
+    print("Number is positive")
+
+elif number < 0:
+    print("Number is negative")
+
+else:
+    print("Number is 0")
